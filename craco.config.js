1bf2e4d6d43d9963b5ab1c0cfe02549ea8d74e3e
Hector Daniel Enriquez Carrasco <hector.enriquez0423@gmail.com>
2026-05-23 00:03:50 -0600

    fix: add webpack configuration to ignore critical dependency warnings

--- craco.config.js
+++ craco.config.js
@@ -4,4 +4,12 @@ module.exports = {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
     },
   },
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.ignoreWarnings = [
+        /Critical dependency: the request of a dependency is an expression/,
+      ];
+      return webpackConfig;
+    },
+  },
 };
